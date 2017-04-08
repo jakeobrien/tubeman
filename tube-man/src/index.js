3b430771954199a3998b7d94b9d29a63f820e4b1
Jake OBrien <neuronrain@gmail.com>
2017-04-08 17:46:17 -0400

    init firebase with config

--- tube-man/src/index.js
+++ tube-man/src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyBCrH3gdhCPNuL8ATHcl88JCYNyfquaz30",
+    authDomain: "tubeman-677a6.firebaseapp.com",
+    databaseURL: "https://tubeman-677a6.firebaseio.com",
+    projectId: "tubeman-677a6",
+    storageBucket: "tubeman-677a6.appspot.com",
+    messagingSenderId: "643336832336"
+  };
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
