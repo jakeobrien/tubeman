8d9f905a37d77fcb957ff5242e023c75abd61015
Jake OBrien <neuronrain@gmail.com>
2017-04-20 13:57:08 -0400

    got rid of unnecessary imports

--- tube-man/src/App.js
+++ tube-man/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { UserStatus, AuthToggle, SetNameForm, WaitingForFightView, MidFightView, MidFightBetView, BetForm, FightOverView, FightOverResult } from './Components.js';
+import { UserStatus, AuthToggle, SetNameForm, WaitingForFightView, MidFightView, FightOverView } from './Components.js';
 import * as firebase from 'firebase';
 
 
