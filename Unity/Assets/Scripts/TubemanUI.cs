a2c4904021913dfcb4b3d929d9b0694f4e8d909d
Jake OBrien <neuronrain@gmail.com>
2017-04-30 09:43:31 -0400

    TubemanUI class

---
+++ Unity/Assets/Scripts/TubemanUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TubemanUI : MonoBehaviour 
+{
+
+    [SerializeField]
+    private Text _nameText;
+    [SerializeField]
+    private Text _potText;
+    [SerializeField]
+    private Text _oddsText;
+    [SerializeField]
+    private Slider _healthSlider;
+    
+    public string Name
+    {
+        set { _nameText.text = value; }
+    }
+
+    public int Pot
+    {
+        set { _potText.text = string.Format("${0}", value); }
+    }
+
+    public string Odds
+    {
+        set { _oddsText.text = value; }
+    }
+
+}
