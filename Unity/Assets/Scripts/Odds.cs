5521ba29adc11aa285f20258b4ff50675cb5d9aa
Jake OBrien <neuronrain@gmail.com>
2017-04-30 16:39:38 -0400

    updated odds to be whole number friendly to bet multiples of 4

--- Unity/Assets/Scripts/Odds.cs
+++ Unity/Assets/Scripts/Odds.cs
@@ -6,25 +6,15 @@ public class Odds
 
     public static List<Odds> Table = new List<Odds>()
     {
-        new Odds { winPercent = 0.91f, oddsString = "1/10" },
-        new Odds { winPercent = 0.89f, oddsString = "1/8" },
-        new Odds { winPercent = 0.86f, oddsString = "1/6" },
-        new Odds { winPercent = 0.83f, oddsString = "1/5" },
-        new Odds { winPercent = 0.80f, oddsString = "1/4" },
-        new Odds { winPercent = 0.75f, oddsString = "1/3" },
+        new Odds { winPercent = 1f, oddsString = "0" },
+        new Odds { winPercent = 0.8f, oddsString = "1/4" },
         new Odds { winPercent = 0.67f, oddsString = "1/2" },
-        new Odds { winPercent = 0.6f,  oddsString = "2/3" },
         new Odds { winPercent = 0.57f, oddsString = "3/4" },
-        new Odds { winPercent = 0.5f,  oddsString = "1/1" },
-        new Odds { winPercent = 0.43f, oddsString = "4/3" },
-        new Odds { winPercent = 0.4f,  oddsString = "3/2" },
+        new Odds { winPercent = 0.5f, oddsString = "1/1" },
+        new Odds { winPercent = 0.4f, oddsString = "3/2" },
         new Odds { winPercent = 0.33f, oddsString = "2/1" },
-        new Odds { winPercent = 0.25f, oddsString = "3/1" },
-        new Odds { winPercent = 0.2f,  oddsString = "4/1" },
-        new Odds { winPercent = 0.17f, oddsString = "5/1" },
-        new Odds { winPercent = 0.14f, oddsString = "6/1" },
-        new Odds { winPercent = 0.11f, oddsString = "8/1" },
-        new Odds { winPercent = 0.09f, oddsString = "10/1" }
+        new Odds { winPercent = 0.20f, oddsString = "4/1" },
+        new Odds { winPercent = 0.11f, oddsString = "8/1" }
     };
 
     public float winPercent;
